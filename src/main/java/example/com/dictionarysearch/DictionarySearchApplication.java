a2c8387c4d6a6bea605f40bb42f4e1198dc07e9e
flrn-dev <136392863+flrn-dev@users.noreply.github.com>
2025-09-17 22:51:25 +0200

    Update and rename WbSearchApplication.java to DictionarySearchApplication.java

--- src/main/java/example/com/dictionarysearch/DictionarySearchApplication.java
+++ src/main/java/example/com/dictionarysearch/DictionarySearchApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WbSearchApplication {
+public class DictionarySearchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WbSearchApplication.class, args);
+		SpringApplication.run(DictionarySearchApplication.class, args);
 	}
 
 }
